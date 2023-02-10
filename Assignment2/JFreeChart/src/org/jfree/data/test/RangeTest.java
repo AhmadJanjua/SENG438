4f293971c378cb0a7fb61f3bfc3841542a785bb2
maxwell-kepler <71337818+maxwell-kepler@users.noreply.github.com>
2023-02-09 19:10:02 -0700

    Max (#13)
    * Create Bug Reporting - Jira - Defect Report.csv
    
    Added bug report
    
    * Create Bug Reporting - Jira - Defect Report.csv
    
    Added correct bug report
    
    * Created DataUtilitiesTest
    
    Updated gitignore
    
    * Added tests
    
    Added tests for calculateRowTotal, calculateColumnTotal, and toString methods

--- Assignment2/JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ Assignment2/JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -3,13 +3,55 @@ package org.jfree.data.test;
 import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.*;
 
 public class RangeTest {
-	
+    private Range range;
     private Range exampleRange;
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
 
     @Before
+    public void setUp() throws Exception {}
+
+
+  
+/////TEST CASES FOR toString
+    @Test
+    public void toStringBothNegative() {
+    	range = new Range(-2.0, -1.0);
+    	String expected = "Range[-2.0,-1.0]";
+    	String actual = range.toString();
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void toStringBothPositive() {
+    	range = new Range(1.0, 5.0);
+    	String expected = "Range[1.0,5.0]";
+    	String actual = range.toString();
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void toStringNegativePositive() {
+    	range = new Range(-5, 3);
+    	String expected = "Range[-5.0,3.0]";
+    	String actual = range.toString();
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void toStringSameValue() {
+    	range = new Range(0,0);
+    	String expected = "Range[0.0,0.0]";
+    	String actual = range.toString();
+    	assertEquals(expected, actual);
+    }
+    
+    
+   
+    @After
+    public void tearDown() throws Exception {
+    	range = null;
     public void setUp() throws Exception { exampleRange = new Range(-1, 1);
     }
 
