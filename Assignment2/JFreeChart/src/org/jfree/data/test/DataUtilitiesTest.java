f6c6282e047fe16c3efe5688bcd17db8f2b9b61d
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2023-02-09 20:25:33 -0700

    Added Test Cases
    Added Test cases to toString() and to getCumulativePercentage().
    Added setup and cleanup to code.

--- Assignment2/JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ Assignment2/JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -1,35 +1,161 @@
 package org.jfree.data.test;
 
-import static org.junit.Assert.*;
-
+import org.jfree.data.DataUtilities;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Values2D;
 
 import java.security.InvalidParameterException;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import static org.junit.Assert.*;
 import org.junit.*;
 
 
-import org.jfree.data.DataUtilities;
-import org.jfree.data.Values2D;
-
 
 public class DataUtilitiesTest extends DataUtilities {
+	// Fields
 	private Mockery mockingContext;
 	private Values2D values;
-	
-	
-	@BeforeClass public static void setUpBeforeClass() throws Exception {}
+	private KeyedValues keyValues;
 
+	// Setup for each test
 	@Before
 	public void setUp() throws Exception {
 		mockingContext = new Mockery();
 		values = mockingContext.mock(Values2D.class);
 	}
+	@After
+    public void tearDown() throws Exception {
+		mockingContext = null;
+		values = null;
+	}
 	
+// TEST CASES FOR: getCumulativePercentage()
+	
+	// Testing for valid Key, value pairs with many entries
+	@Test
+	public void cummulativePercentValidManyItem() {
+		mockingContext.checking(new Expectations() {
+			{
+				// Number of items
+				allowing(keyValues).getItemCount();
+				will(returnValue(3));
+				
+				// getKey
+				allowing(keyValues).getKey(0);
+				will(returnValue(0));
+				allowing(keyValues).getKey(1);
+				will(returnValue(1));
+				allowing(keyValues).getKey(2);
+				will(returnValue(2));
+				
+				// getIndex
+				allowing(keyValues).getIndex(0);
+				will(returnValue(0));
+				allowing(keyValues).getIndex(1);
+				will(returnValue(1));
+				allowing(keyValues).getIndex(2);
+				will(returnValue(2));
+				
+				// getValue
+				allowing(keyValues).getValue(0);
+				will(returnValue(5));
+				allowing(keyValues).getValue(1);
+				will(returnValue(9));
+				allowing(keyValues).getValue(2);
+				will(returnValue(2));	
+			}
+		});
+	    KeyedValues actual = DataUtilities.getCumulativePercentages(keyValues);
+	    assertTrue(actual.equals(keyValues));
+	}
+	
+	// Testing for valid Key, value pairs with single entries
+	@Test
+	public void cummulativePercentValidOneItem() {
+		mockingContext.checking(new Expectations() {
+			{
+				// Number of items
+				allowing(keyValues).getItemCount();
+				will(returnValue(1));
+				
+				// getKey
+				allowing(keyValues).getKey(0);
+				will(returnValue(0));
+				
+				// getIndex
+				allowing(keyValues).getIndex(0);
+				will(returnValue(0));
 
-	/////TEST CASES FOR calculateColumnTotal
+				// getValue
+				allowing(keyValues).getValue(0);
+				will(returnValue(5));	
+			}
+		});
+	    KeyedValues actual = DataUtilities.getCumulativePercentages(keyValues);
+	    assertTrue(actual.equals(keyValues));
+	}
+	
+	// Test for key, value pair with no entries.
+	@Test
+	public void cummulativePercentValidEmpty() {
+		mockingContext.checking(new Expectations() {
+			{
+				// Number of items
+				allowing(keyValues).getItemCount();
+				will(returnValue(0));
+			}
+		});
+	    KeyedValues actual = DataUtilities.getCumulativePercentages(keyValues);
+	    assertTrue(actual.equals(keyValues));
+	}
+	
+	// Test for key value pair with corrupted entry
+	@Test ( expected = InvalidParameterException.class)
+	public void cummulativePercentInvalid() {
+	    mockingContext.checking(new Expectations() {
+	        {
+				// Number of items
+				allowing(keyValues).getItemCount();
+				will(returnValue(1));
+				
+				// getKey
+				allowing(keyValues).getKey(0);
+				will(returnValue(0));
+				
+				// getIndex
+				allowing(keyValues).getIndex(0);
+				will(returnValue(0));
+				
+				// getValue
+				allowing(keyValues).getValue(0);
+				will(returnValue("1"));
+	        }
+	    });
 
+	    try {
+	        DataUtilities.getCumulativePercentages(keyValues);
+	        fail("The InvalidParameterException was not thrown.");
+	    } catch (InvalidParameterException e) {
+	        return;
+	    }
+	    catch(Exception e) {}
+	}
+	
+	// Test for null input to method
+	@Test ( expected = InvalidParameterException.class)
+	public void cummulativePercentNull() {
+	    try {
+	        DataUtilities.getCumulativePercentages(null);
+	        fail("The InvalidParameterException was not thrown.");
+	    } catch (InvalidParameterException e) {
+	        return;
+	    }
+	    catch(Exception e) {}
+	}
+	
+// TEST CASES FOR: calculateColumnTotal
 	@Test
 	public void calculateColumnTotalForTwoValues() {
 		mockingContext.checking(new Expectations() {
@@ -113,13 +239,7 @@ public class DataUtilitiesTest extends DataUtilities {
 		fail();
 	}*/
 	
-	
-	
-	
-	
-	
-	
-    /////TEST CASES FOR calculateRowTotal
+// TEST CASES FOR: calculateRowTotal
 	@Test
 	public void calculateRowTotalForTwoValues() {
 		mockingContext.checking(new Expectations() {
@@ -164,47 +284,38 @@ public class DataUtilitiesTest extends DataUtilities {
 	    assertEquals(expected, actual, .000000001d);
 	}
 
-	
-	
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void calculateRowTotalForInvalidIndex(){
-		mockingContext.checking(new Expectations() {
-	        {
-	        	one(values).getRowCount();
-	        	will(returnValue(1));
-	            one(values).getColumnCount();
-	        	will(returnValue(1));
-	            one(values).getValue(-1, 0);
-	            will(throwException(new IndexOutOfBoundsException()));         
-	        }
-	    });
-		int row = -1;
-		double actual = DataUtilities.calculateRowTotal(values, row);
-	    double expected = 0.0;
-	    assertEquals(expected, actual, .000000001d);
-	    //fail();
-	            //one(values).getRowCount();
-	        	//will(returnValue(2));
-	            one(values).getColumnCount();
-	        	will(returnValue(1));
-	            one(values).getValue(0, 0);
-	            will(returnValue(7.5));
-	            one(values).getValue(1, 0);
-	            will(returnValue(2.5));
-	        }
-	    });
-	    double actual = DataUtilities.calculateColumnTotal(values, 0);
-	    double expected = 10.0;
-	    assertEquals(actual, expected, .000000001d);
-	}
-	
-	@After
-    public void tearDown() throws Exception {
-		mockingContext = null;
-		values = null;
-	}
+//	@Test(expected = IndexOutOfBoundsException.class)
+//	public void calculateRowTotalForInvalidIndex(){
+//		mockingContext.checking(new Expectations() {
+//	        {
+//	        	one(values).getRowCount();
+//	        	will(returnValue(1));
+//	            one(values).getColumnCount();
+//	        	will(returnValue(1));
+//	            one(values).getValue(-1, 0);
+//	            will(throwException(new IndexOutOfBoundsException()));         
+//	        }
+//	    });
+//		int row = -1;
+//		double actual = DataUtilities.calculateRowTotal(values, row);
+//	    double expected = 0.0;
+//	    assertEquals(expected, actual, .000000001d);
+//	    //fail();
+//	            //one(values).getRowCount();
+//	        	//will(returnValue(2));
+//	            one(values).getColumnCount();
+//	        	will(returnValue(1));
+//	            one(values).getValue(0, 0);
+//	            will(returnValue(7.5));
+//	            one(values).getValue(1, 0);
+//	            will(returnValue(2.5));
+//	        }
+//	    });
+//	    double actual = DataUtilities.calculateColumnTotal(values, 0);
+//	    double expected = 10.0;
+//	    assertEquals(actual, expected, .000000001d);
+//	}
 	
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {}
+
 }
